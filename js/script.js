56ea8a5113f9514f467f0761651eadb0ec8aad34
Roman Olyvko <romanolyvko1991@gmail.com>
2020-09-25 18:07:54 +0300

    my first commit in portfolio

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('Main_page_2').style.display="none";
+document.getElementById('button').onclick=function(){
+    document.getElementById('Main_page_1').style.display="none";
+    document.location.href = "https://romanolyvko.lviv.ua/";
+}
